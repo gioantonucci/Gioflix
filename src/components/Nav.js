ed8ed3ceffb60a94ace628d6c56dc409c2454946
Giovanna Antonucci <97487819+gioantonucci@users.noreply.github.com>
2022-06-15 16:41:12 -0300

    update nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,26 +4,27 @@ import avatar from "../images/avatar.png"
 import '../styles/Nav.css'
 
 export default function Nav() {
-//   const [show, handleShow] = useState(false);
+   const [show, handleShow] = useState(false);
 
 
-//   useEffect(()=> {
-//     window.addEventListener("scroll", () => {
-//       if (window.scrollY > 100) {
-//         handleShow(true)
-//       } else handleShow(false)
-//     });
+   useEffect(()=> {
+     window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+         handleShow(true)
+       } else handleShow(false)
+     });
     
-//   return () => {
-//     window.removeEventListener("scroll")
-//   };
-// }, []);
+   return () => {
+     window.removeEventListener("scroll", null)
+   };
+ }, []);
   
   return (
-    //<div className={`nav ${show && 'nav_black'}`}>
+    <div className={`nav ${show && 'nav_black'}`}>
       <div className="nav">
       <img className="nav_logo" src={logo} alt="gioflix logo" />
       <img className="nav_avatar" src={avatar} alt="about" />
+    </div>
     </div>
   );
 }
